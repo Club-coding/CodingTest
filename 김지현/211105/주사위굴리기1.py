963971d183d4bb9cfbc75c26acf07f4a36a57deb
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2021-11-06 00:37:40 +0900

    jh 211105

---
+++ 김지현/211105/주사위굴리기1.py
@@ -0,0 +1,9 @@
+import sys
+
+intput = sys.stdin.readline
+dice = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+n, m, x, y, k = map(int, input().split())
+for i in n:
+    maps.append(list(map(int, input().split())))
+com = list(map(int, input().split()))
+print(maps)
